e5cc41bc91982b36c4b9a2740b9847243252a0cf
ujjwal G <crystalninja0123@gmail.com>
2026-02-04 19:44:58 +0530

    chess board using turtle

--- chess.py
+++ chess.py
@@ -6,8 +6,22 @@ t.goto(-200, -200)
 t.pendown()
 y = -200
 x = -200
+color = "white"
 for boxes in range(8):
+    if color == "black":
+        t.fillcolor(color)
+        color = "white"
+    else:
+        t.fillcolor(color)
+        color = "black"
     for box in range(8):
+        if color == "black":
+            t.fillcolor(color)
+            color = "white"
+        else:
+            t.fillcolor(color)
+            color = "black"
+        t.begin_fill()
         t.forward(40)
         t.left(90)
         turtle.forward(40)
@@ -17,8 +31,11 @@ for boxes in range(8):
         turtle.forward(40)
         turtle.left(90)
         turtle.forward(40)
+        t.end_fill()
 
     t.penup()
     y += 40
     t.goto(x, y)
     t.pendown()
+
+turtle.done()
