015c475e38a915fe98fd5bd5198d5c5834a3452b
ujjwal G <crystalninja0123@gmail.com>
2026-02-05 23:09:50 +0530

    better implementation and board logic

--- chess.py
+++ chess.py
@@ -1,41 +1,70 @@
 import turtle as t
-import turtle
 
-t.penup()
-t.goto(-200, -200)
-t.pendown()
-y = -200
-x = -200
+window = t.Screen()
+window.setup(800, 800)
+board = t.Turtle()
+board.penup()
+board.goto(-400, -400)
+board.pendown()
+y = -400
+x = -400
+board.speed(0)
+window.register_shape("./pieces/pawn-b.gif")
+window.register_shape("./pieces/pawn-w.gif")
+window.register_shape("./pieces/bishop-b.gif")
+window.register_shape("./pieces/bishop-w.gif")
+window.register_shape("./pieces/knight-w.gif")
+window.register_shape("./pieces/knight-b.gif")
+window.register_shape("./pieces/rook-b.gif")
+window.register_shape("./pieces/rook-w.gif")
+window.register_shape("./pieces/queen-b.gif")
+window.register_shape("./pieces/queen-w.gif")
+window.register_shape("./pieces/king-b.gif")
+window.register_shape("./pieces/king-w.gif")
+
+
 color = "white"
 for boxes in range(8):
-    if color == "black":
-        t.fillcolor(color)
-        color = "white"
-    else:
-        t.fillcolor(color)
-        color = "black"
     for box in range(8):
-        if color == "black":
-            t.fillcolor(color)
-            color = "white"
+        if (boxes + box) % 2 == 0:
+            board.fillcolor("black")
         else:
-            t.fillcolor(color)
-            color = "black"
-        t.begin_fill()
-        t.forward(40)
-        t.left(90)
-        turtle.forward(40)
-        turtle.left(90)
-        turtle.forward(40)
-        turtle.left(90)
-        turtle.forward(40)
-        turtle.left(90)
-        turtle.forward(40)
-        t.end_fill()
+            board.fillcolor("white")
+        board.begin_fill()
+        board.forward(100)
+        board.left(90)
+        board.forward(100)
+        board.left(90)
+        board.forward(100)
+        board.left(90)
+        board.forward(100)
+        board.left(90)
+        board.end_fill()
+        board.penup()
+        board.forward(100)
+        board.pendown()
+    board.penup()
+    y += 100
+    board.goto(x, y)
+    board.pendown()
+board.hideturtle()
 
-    t.penup()
-    y += 40
-    t.goto(x, y)
-    t.pendown()
+rook_w = t.Turtle()
+rook_w.shape("./pieces/rook-w.gif")
 
-turtle.done()
+rook_b = t.Turtle()
+rook_w.shape("./pieces/rook-b.gif")
+rook_b.penup()
+rook_w.penup()
+
+
+def drag_handler(x, y):
+    rook_w.goto(x, y)
+    t.update()
+
+
+t.hideturtle()
+rook_w.ondrag(drag_handler)
+
+t.tracer(0)
+t.done()
