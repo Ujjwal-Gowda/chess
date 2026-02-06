4a2a83084001c177a4a8144c21f94b5fae07b268
ujjwal G <crystalninja0123@gmail.com>
2026-02-06 13:52:57 +0530

    added all the pieces

--- chess.py
+++ chess.py
@@ -2,6 +2,7 @@ import turtle as t
 
 window = t.Screen()
 window.setup(800, 800)
+t.tracer(0)
 board = t.Turtle()
 board.penup()
 board.goto(-400, -400)
@@ -46,24 +47,77 @@ for boxes in range(8):
     y += 100
     board.goto(x, y)
     board.pendown()
+board.penup()
 board.hideturtle()
 
 rook_w = t.Turtle()
 rook_w.shape("./pieces/rook-w.gif")
-
-rook_b = t.Turtle()
-rook_w.shape("./pieces/rook-b.gif")
-rook_b.penup()
 rook_w.penup()
 
+rook_b = t.Turtle()
+rook_b.shape("./pieces/rook-b.gif")
+rook_b.penup()
 
-def drag_handler(x, y):
-    rook_w.goto(x, y)
+
+bishop_w = t.Turtle()
+bishop_w.shape("./pieces/bishop-w.gif")
+bishop_w.penup()
+
+
+bishop_b = t.Turtle()
+bishop_b.shape("./pieces/bishop-b.gif")
+bishop_b.penup()
+
+knight_b = t.Turtle()
+knight_b.shape("./pieces/knight-b.gif")
+knight_b.penup()
+
+knight_w = t.Turtle()
+knight_w.shape("./pieces/knight-w.gif")
+knight_w.penup()
+
+king_w = t.Turtle()
+king_w.shape("./pieces/king-w.gif")
+king_w.penup()
+
+king_b = t.Turtle()
+king_b.shape("./pieces/king-b.gif")
+king_b.penup()
+
+queen_b = t.Turtle()
+queen_b.shape("./pieces/queen-b.gif")
+queen_b.penup()
+
+queen_w = t.Turtle()
+queen_w.shape("./pieces/queen-w.gif")
+queen_w.penup()
+
+pawn_w = t.Turtle()
+pawn_w.shape("./pieces/pawn-w.gif")
+pawn_w.penup()
+
+pawn_b = t.Turtle()
+pawn_b.shape("./pieces/pawn-b.gif")
+pawn_b.penup()
+
+
+def drag_handler(x, y, name):
+    name.goto(x, y)
     t.update()
 
 
 t.hideturtle()
-rook_w.ondrag(drag_handler)
-
-t.tracer(0)
+pawn_w.ondrag(lambda x, y: drag_handler(x, y, pawn_w))
+pawn_b.ondrag(lambda x, y: drag_handler(x, y, pawn_b))
+rook_w.ondrag(lambda x, y: drag_handler(x, y, rook_w))
+rook_b.ondrag(lambda x, y: drag_handler(x, y, rook_b))
+bishop_b.ondrag(lambda x, y: drag_handler(x, y, bishop_b))
+bishop_w.ondrag(lambda x, y: drag_handler(x, y, bishop_w))
+knight_w.ondrag(lambda x, y: drag_handler(x, y, knight_w))
+knight_b.ondrag(lambda x, y: drag_handler(x, y, knight_b))
+king_b.ondrag(lambda x, y: drag_handler(x, y, king_b))
+king_w.ondrag(lambda x, y: drag_handler(x, y, king_w))
+queen_b.ondrag(lambda x, y: drag_handler(x, y, queen_b))
+queen_w.ondrag(lambda x, y: drag_handler(x, y, queen_w))
+t.update()
 t.done()
