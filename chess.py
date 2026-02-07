d609c1ce4a2a74a29aa3b6908ad587d3ffd7cb40
ujjwal G <crystalninja0123@gmail.com>
2026-02-07 20:41:11 +0530

    imporvision of position and removed drag approach

--- chess.py
+++ chess.py
@@ -1,4 +1,5 @@
 import turtle as t
+import math
 
 window = t.Screen()
 window.setup(800, 800)
@@ -22,6 +23,16 @@ window.register_shape("./pieces/queen-b.gif")
 window.register_shape("./pieces/queen-w.gif")
 window.register_shape("./pieces/king-b.gif")
 window.register_shape("./pieces/king-w.gif")
+
+pieces = []
+
+
+class Piece:
+    def __init__(self, kind, color, position, turtle):
+        self.kind = (kind,)
+        self.color = (color,)
+        self.position = (position,)
+        self.turtle = (turtle,)
 
 
 for boxes in range(8):
@@ -51,72 +62,81 @@ board.penup()
 board.hideturtle()
 
 
-boardbox = {
-    "a1": {"occupied": False, "cordinates": [-350, -350]},
-    "b1": {"occupied": False, "cordinates": [-250, -350]},
-    "c1": {"occupied": False, "cordinates": [-150, -350]},
-    "d1": {"occupied": False, "cordinates": [-50, -350]},
-    "e1": {"occupied": False, "cordinates": [50, -350]},
-    "f1": {"occupied": False, "cordinates": [150, -350]},
-    "g1": {"occupied": False, "cordinates": [250, -350]},
-    "h1": {"occupied": False, "cordinates": [350, -350]},
-    "a2": {"occupied": False, "cordinates": [-350, -250]},
-    "b2": {"occupied": False, "cordinates": [-250, -250]},
-    "c2": {"occupied": False, "cordinates": [-150, -250]},
-    "d2": {"occupied": False, "cordinates": [-50, -250]},
-    "e2": {"occupied": False, "cordinates": [50, -250]},
-    "f2": {"occupied": False, "cordinates": [150, -250]},
-    "g2": {"occupied": False, "cordinates": [250, -250]},
-    "h2": {"occupied": False, "cordinates": [350, -250]},
-    "a3": {"occupied": False, "cordinates": [-350, -150]},
-    "b3": {"occupied": False, "cordinates": [-250, -150]},
-    "c3": {"occupied": False, "cordinates": [-150, -150]},
-    "d3": {"occupied": False, "cordinates": [-50, -150]},
-    "e3": {"occupied": False, "cordinates": [50, -150]},
-    "f3": {"occupied": False, "cordinates": [150, -150]},
-    "g3": {"occupied": False, "cordinates": [250, -150]},
-    "h3": {"occupied": False, "cordinates": [350, -150]},
-    "a4": {"occupied": False, "cordinates": [-350, -50]},
-    "b4": {"occupied": False, "cordinates": [-250, -50]},
-    "c4": {"occupied": False, "cordinates": [-150, -50]},
-    "d4": {"occupied": False, "cordinates": [-50, -50]},
-    "e4": {"occupied": False, "cordinates": [50, -50]},
-    "f4": {"occupied": False, "cordinates": [150, -50]},
-    "g4": {"occupied": False, "cordinates": [250, -50]},
-    "h4": {"occupied": False, "cordinates": [350, -50]},
-    "a5": {"occupied": False, "cordinates": [-350, 50]},
-    "b5": {"occupied": False, "cordinates": [-250, 50]},
-    "c5": {"occupied": False, "cordinates": [-150, 50]},
-    "d5": {"occupied": False, "cordinates": [-50, 50]},
-    "e5": {"occupied": False, "cordinates": [50, 50]},
-    "f5": {"occupied": False, "cordinates": [150, 50]},
-    "g5": {"occupied": False, "cordinates": [250, 50]},
-    "h5": {"occupied": False, "cordinates": [350, 50]},
-    "a6": {"occupied": False, "cordinates": [-350, 150]},
-    "b6": {"occupied": False, "cordinates": [-250, 150]},
-    "c6": {"occupied": False, "cordinates": [-150, 150]},
-    "d6": {"occupied": False, "cordinates": [-50, 150]},
-    "e6": {"occupied": False, "cordinates": [50, 150]},
-    "f6": {"occupied": False, "cordinates": [150, 150]},
-    "g6": {"occupied": False, "cordinates": [250, 150]},
-    "h6": {"occupied": False, "cordinates": [350, 150]},
-    "a7": {"occupied": False, "cordinates": [-350, 250]},
-    "b7": {"occupied": False, "cordinates": [-250, 250]},
-    "c7": {"occupied": False, "cordinates": [-150, 250]},
-    "d7": {"occupied": False, "cordinates": [-50, 250]},
-    "e7": {"occupied": False, "cordinates": [50, 250]},
-    "f7": {"occupied": False, "cordinates": [150, 250]},
-    "g7": {"occupied": False, "cordinates": [250, 250]},
-    "h7": {"occupied": False, "cordinates": [350, 250]},
-    "a8": {"occupied": False, "cordinates": [-350, 350]},
-    "b8": {"occupied": False, "cordinates": [-250, 350]},
-    "c8": {"occupied": False, "cordinates": [-150, 350]},
-    "d8": {"occupied": False, "cordinates": [-50, 350]},
-    "e8": {"occupied": False, "cordinates": [50, 350]},
-    "f8": {"occupied": False, "cordinates": [150, 350]},
-    "g8": {"occupied": False, "cordinates": [250, 350]},
-    "h8": {"occupied": False, "cordinates": [350, 350]},
-}
+# boardbox = {
+#     "a1": {"occupied": False, "cordinates": [-350, -350]},
+#     "b1": {"occupied": False, "cordinates": [-250, -350]},
+#     "c1": {"occupied": False, "cordinates": [-150, -350]},
+#     "d1": {"occupied": False, "cordinates": [-50, -350]},
+#     "e1": {"occupied": False, "cordinates": [50, -350]},
+#     "f1": {"occupied": False, "cordinates": [150, -350]},
+#     "g1": {"occupied": False, "cordinates": [250, -350]},
+#     "h1": {"occupied": False, "cordinates": [350, -350]},
+#     "a2": {"occupied": False, "cordinates": [-350, -250]},
+#     "b2": {"occupied": False, "cordinates": [-250, -250]},
+#     "c2": {"occupied": False, "cordinates": [-150, -250]},
+#     "d2": {"occupied": False, "cordinates": [-50, -250]},
+#     "e2": {"occupied": False, "cordinates": [50, -250]},
+#     "f2": {"occupied": False, "cordinates": [150, -250]},
+#     "g2": {"occupied": False, "cordinates": [250, -250]},
+#     "h2": {"occupied": False, "cordinates": [350, -250]},
+#     "a3": {"occupied": False, "cordinates": [-350, -150]},
+#     "b3": {"occupied": False, "cordinates": [-250, -150]},
+#     "c3": {"occupied": False, "cordinates": [-150, -150]},
+#     "d3": {"occupied": False, "cordinates": [-50, -150]},
+#     "e3": {"occupied": False, "cordinates": [50, -150]},
+#     "f3": {"occupied": False, "cordinates": [150, -150]},
+#     "g3": {"occupied": False, "cordinates": [250, -150]},
+#     "h3": {"occupied": False, "cordinates": [350, -150]},
+#     "a4": {"occupied": False, "cordinates": [-350, -50]},
+#     "b4": {"occupied": False, "cordinates": [-250, -50]},
+#     "c4": {"occupied": False, "cordinates": [-150, -50]},
+#     "d4": {"occupied": False, "cordinates": [-50, -50]},
+#     "e4": {"occupied": False, "cordinates": [50, -50]},
+#     "f4": {"occupied": False, "cordinates": [150, -50]},
+#     "g4": {"occupied": False, "cordinates": [250, -50]},
+#     "h4": {"occupied": False, "cordinates": [350, -50]},
+#     "a5": {"occupied": False, "cordinates": [-350, 50]},
+#     "b5": {"occupied": False, "cordinates": [-250, 50]},
+#     "c5": {"occupied": False, "cordinates": [-150, 50]},
+#     "d5": {"occupied": False, "cordinates": [-50, 50]},
+#     "e5": {"occupied": False, "cordinates": [50, 50]},
+#     "f5": {"occupied": False, "cordinates": [150, 50]},
+#     "g5": {"occupied": False, "cordinates": [250, 50]},
+#     "h5": {"occupied": False, "cordinates": [350, 50]},
+#     "a6": {"occupied": False, "cordinates": [-350, 150]},
+#     "b6": {"occupied": False, "cordinates": [-250, 150]},
+#     "c6": {"occupied": False, "cordinates": [-150, 150]},
+#     "d6": {"occupied": False, "cordinates": [-50, 150]},
+#     "e6": {"occupied": False, "cordinates": [50, 150]},
+#     "f6": {"occupied": False, "cordinates": [150, 150]},
+#     "g6": {"occupied": False, "cordinates": [250, 150]},
+#     "h6": {"occupied": False, "cordinates": [350, 150]},
+#     "a7": {"occupied": False, "cordinates": [-350, 250]},
+#     "b7": {"occupied": False, "cordinates": [-250, 250]},
+#     "c7": {"occupied": False, "cordinates": [-150, 250]},
+#     "d7": {"occupied": False, "cordinates": [-50, 250]},
+#     "e7": {"occupied": False, "cordinates": [50, 250]},
+#     "f7": {"occupied": False, "cordinates": [150, 250]},
+#     "g7": {"occupied": False, "cordinates": [250, 250]},
+#     "h7": {"occupied": False, "cordinates": [350, 250]},
+#     "a8": {"occupied": False, "cordinates": [-350, 350]},
+#     "b8": {"occupied": False, "cordinates": [-250, 350]},
+#     "c8": {"occupied": False, "cordinates": [-150, 350]},
+#     "d8": {"occupied": False, "cordinates": [-50, 350]},
+#     "e8": {"occupied": False, "cordinates": [50, 350]},
+#     "f8": {"occupied": False, "cordinates": [150, 350]},
+#     "g8": {"occupied": False, "cordinates": [250, 350]},
+#     "h8": {"occupied": False, "cordinates": [350, 350]},
+# }
+
+FILES = "abcdefgh"
+RANKS = "12345678"
+
+
+def square_to_xy(file, rank):
+    x = -350 + FILES.index(file) * 100
+    y = -350 + (int(rank) - 1) * 100
+    return x, y
 
 
 rook_w_1 = t.Turtle()
@@ -250,59 +270,88 @@ pawn_b_8.shape("./pieces/pawn-b.gif")
 pawn_b_8.penup()
 
 
+king = Piece("king", "white", "e8", king_w)
+pieces.append(king)
+
+king = Piece("king", "black", "e1", king_b)
+pieces.append(king)
+
+queen = Piece("queen", "black", "d1", queen_b)
+pieces.append(queen)
+
+queen = Piece("queen", "white", "d8", queen_w)
+pieces.append(queen)
+
+pawn = Piece("pawn", "black", "a2", pawn_b_1)
+pieces.append(queen)
+
+boards = {}
+boards["e1"] = pawn
+boards["e1"] = king
+newx = 0
+newy = 0
+
+
 def drag_handler(x, y, name):
     name.goto(x, y)
+    newx = round_to_nearest(x, 50)
+    newy = round_to_nearest(y, 50)
+    if (newx / 50) % 2 == 0:
+        newy += 50
+    print(newx, newy)
+    name.goto(newx, newy)
     t.update()
 
 
-rook_w_1.penup()
-rook_w_1.goto(-150, -150)
-t.hideturtle()
-
-pawn_w_1.ondrag(lambda x, y: drag_handler(x, y, pawn_w_1))
-pawn_w_2.ondrag(lambda x, y: drag_handler(x, y, pawn_w_2))
-pawn_w_3.ondrag(lambda x, y: drag_handler(x, y, pawn_w_3))
-pawn_w_4.ondrag(lambda x, y: drag_handler(x, y, pawn_w_4))
-pawn_w_5.ondrag(lambda x, y: drag_handler(x, y, pawn_w_5))
-pawn_w_6.ondrag(lambda x, y: drag_handler(x, y, pawn_w_6))
-pawn_w_7.ondrag(lambda x, y: drag_handler(x, y, pawn_w_7))
-pawn_w_8.ondrag(lambda x, y: drag_handler(x, y, pawn_w_8))
+def round_to_nearest(n, base):
+    return round(n / base) * base
 
 
-pawn_b_1.ondrag(lambda x, y: drag_handler(x, y, pawn_b_1))
-pawn_b_2.ondrag(lambda x, y: drag_handler(x, y, pawn_b_2))
-pawn_b_3.ondrag(lambda x, y: drag_handler(x, y, pawn_b_3))
-pawn_b_4.ondrag(lambda x, y: drag_handler(x, y, pawn_b_4))
-pawn_b_5.ondrag(lambda x, y: drag_handler(x, y, pawn_b_5))
-pawn_b_6.ondrag(lambda x, y: drag_handler(x, y, pawn_b_6))
-pawn_b_7.ondrag(lambda x, y: drag_handler(x, y, pawn_b_7))
-pawn_b_8.ondrag(lambda x, y: drag_handler(x, y, pawn_b_8))
+# pawn_w_1.ondrag(lambda x, y: drag_handler(x, y, pawn_w_1))
+# pawn_w_2.ondrag(lambda x, y: drag_handler(x, y, pawn_w_2))
+# pawn_w_3.ondrag(lambda x, y: drag_handler(x, y, pawn_w_3))
+# pawn_w_4.ondrag(lambda x, y: drag_handler(x, y, pawn_w_4))
+# pawn_w_5.ondrag(lambda x, y: drag_handler(x, y, pawn_w_5))
+# pawn_w_6.ondrag(lambda x, y: drag_handler(x, y, pawn_w_6))
+# pawn_w_7.ondrag(lambda x, y: drag_handler(x, y, pawn_w_7))
+# pawn_w_8.ondrag(lambda x, y: drag_handler(x, y, pawn_w_8))
+#
+#
+# pawn_b_1.ondrag(lambda x, y: drag_handler(x, y, pawn_b_1))
+# pawn_b_2.ondrag(lambda x, y: drag_handler(x, y, pawn_b_2))
+# pawn_b_3.ondrag(lambda x, y: drag_handler(x, y, pawn_b_3))
+# pawn_b_4.ondrag(lambda x, y: drag_handler(x, y, pawn_b_4))
+# pawn_b_5.ondrag(lambda x, y: drag_handler(x, y, pawn_b_5))
+# pawn_b_6.ondrag(lambda x, y: drag_handler(x, y, pawn_b_6))
+# pawn_b_7.ondrag(lambda x, y: drag_handler(x, y, pawn_b_7))
+# pawn_b_8.ondrag(lambda x, y: drag_handler(x, y, pawn_b_8))
+#
+# rook_w_1.ondrag(lambda x, y: drag_handler(x, y, rook_w_1))
+# rook_w_2.ondrag(lambda x, y: drag_handler(x, y, rook_w_2))
+#
+# rook_b_1.ondrag(lambda x, y: drag_handler(x, y, rook_b_1))
+# rook_b_2.ondrag(lambda x, y: drag_handler(x, y, rook_b_2))
+#
+# bishop_b_1.ondrag(lambda x, y: drag_handler(x, y, bishop_b_1))
+# bishop_b_2.ondrag(lambda x, y: drag_handler(x, y, bishop_b_2))
+#
+# bishop_w_1.ondrag(lambda x, y: drag_handler(x, y, bishop_w_1))
+# bishop_w_2.ondrag(lambda x, y: drag_handler(x, y, bishop_w_2))
+#
+# knight_w_1.ondrag(lambda x, y: drag_handler(x, y, knight_w_1))
+# knight_w_2.ondrag(lambda x, y: drag_handler(x, y, knight_w_2))
+#
+# knight_b_1.ondrag(lambda x, y: drag_handler(x, y, knight_b_1))
+# knight_b_2.ondrag(lambda x, y: drag_handler(x, y, knight_b_2))
+#
+# king_b.ondrag(lambda x, y: drag_handler(x, y, king_b))
+# king_w.ondrag(lambda x, y: drag_handler(x, y, king_w))
+#
+# queen_b.ondrag(lambda x, y: drag_handler(x, y, queen_b))
+# queen_w.ondrag(lambda x, y: drag_handler(x, y, queen_w))
+#
 
-rook_w_1.ondrag(lambda x, y: drag_handler(x, y, rook_w_1))
-rook_w_2.ondrag(lambda x, y: drag_handler(x, y, rook_w_2))
-
-rook_b_1.ondrag(lambda x, y: drag_handler(x, y, rook_b_1))
-rook_b_2.ondrag(lambda x, y: drag_handler(x, y, rook_b_2))
-
-bishop_b_1.ondrag(lambda x, y: drag_handler(x, y, bishop_b_1))
-bishop_b_2.ondrag(lambda x, y: drag_handler(x, y, bishop_b_2))
-
-bishop_w_1.ondrag(lambda x, y: drag_handler(x, y, bishop_w_1))
-bishop_w_2.ondrag(lambda x, y: drag_handler(x, y, bishop_w_2))
-
-knight_w_1.ondrag(lambda x, y: drag_handler(x, y, knight_w_1))
-knight_w_2.ondrag(lambda x, y: drag_handler(x, y, knight_w_2))
-
-knight_b_1.ondrag(lambda x, y: drag_handler(x, y, knight_b_1))
-knight_b_2.ondrag(lambda x, y: drag_handler(x, y, knight_b_2))
-
-king_b.ondrag(lambda x, y: drag_handler(x, y, king_b))
-king_w.ondrag(lambda x, y: drag_handler(x, y, king_w))
-
-queen_b.ondrag(lambda x, y: drag_handler(x, y, queen_b))
-queen_w.ondrag(lambda x, y: drag_handler(x, y, queen_w))
-
-columns = ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"]
+columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
 pawns_b = [
     pawn_b_1,
     pawn_b_2,
@@ -315,12 +364,11 @@ pawns_b = [
 ]
 for x in range(8):
     pawns_b[x].penup()
-    pawns_b[x].goto(
-        boardbox[columns[x]]["cordinates"][0], boardbox[columns[x]]["cordinates"][1]
-    )
+    alph = columns[x]
+    a, n = square_to_xy(alph, 2)
+    pawns_b[x].goto(a, n)
 
 
-columns = ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]
 pawns_w = [
     pawn_w_1,
     pawn_w_2,
@@ -333,56 +381,74 @@ pawns_w = [
 ]
 for x in range(8):
     pawns_w[x].penup()
-    pawns_w[x].goto(
-        boardbox[columns[x]]["cordinates"][0], boardbox[columns[x]]["cordinates"][1]
-    )
+    alph = columns[x]
+    a, n = square_to_xy(alph, 7)
+    pawns_w[x].goto(a, n)
 rook_w_1.penup()
-rook_w_1.goto(boardbox["a8"]["cordinates"][0], boardbox["a8"]["cordinates"][1])
+a, n = square_to_xy("a", 8)
+rook_w_1.goto(a, n)
+
 
 rook_w_2.penup()
-rook_w_2.goto(boardbox["h8"]["cordinates"][0], boardbox["a8"]["cordinates"][1])
+a, n = square_to_xy("h", 8)
+rook_w_2.goto(a, n)
 
 rook_b_1.penup()
-rook_b_1.goto(boardbox["a1"]["cordinates"][0], boardbox["a1"]["cordinates"][1])
+a, n = square_to_xy("a", 1)
+rook_b_1.goto(a, n)
 
 rook_b_2.penup()
-rook_b_2.goto(boardbox["h1"]["cordinates"][0], boardbox["a1"]["cordinates"][1])
+a, n = square_to_xy("h", 1)
+rook_b_2.goto(a, n)
 
 knight_w_1.penup()
-knight_w_1.goto(boardbox["b8"]["cordinates"][0], boardbox["b8"]["cordinates"][1])
+a, n = square_to_xy("b", 8)
+knight_w_1.goto(a, n)
 
 knight_w_2.penup()
-knight_w_2.goto(boardbox["g8"]["cordinates"][0], boardbox["g8"]["cordinates"][1])
+a, n = square_to_xy("g", 8)
+knight_w_2.goto(a, n)
 
 knight_b_1.penup()
-knight_b_1.goto(boardbox["b1"]["cordinates"][0], boardbox["b1"]["cordinates"][1])
+a, n = square_to_xy("b", 1)
+knight_b_1.goto(a, n)
 
 knight_b_2.penup()
-knight_b_2.goto(boardbox["g1"]["cordinates"][0], boardbox["g1"]["cordinates"][1])
+a, n = square_to_xy("g", 1)
+knight_b_2.goto(a, n)
 
 bishop_w_1.penup()
-bishop_w_1.goto(boardbox["c8"]["cordinates"][0], boardbox["c8"]["cordinates"][1])
+a, n = square_to_xy("c", 8)
+bishop_w_1.goto(a, n)
 
 bishop_w_2.penup()
-bishop_w_2.goto(boardbox["f8"]["cordinates"][0], boardbox["f8"]["cordinates"][1])
+a, n = square_to_xy("f", 8)
+bishop_w_2.goto(a, n)
 
 bishop_b_1.penup()
-bishop_b_1.goto(boardbox["c1"]["cordinates"][0], boardbox["c1"]["cordinates"][1])
+a, n = square_to_xy("c", 1)
+bishop_b_1.goto(a, n)
 
 bishop_b_2.penup()
-bishop_b_2.goto(boardbox["f1"]["cordinates"][0], boardbox["f1"]["cordinates"][1])
+a, n = square_to_xy("f", 1)
+bishop_b_2.goto(a, n)
 
 king_w.penup()
-king_w.goto(boardbox["e8"]["cordinates"][0], boardbox["e8"]["cordinates"][1])
+a, n = square_to_xy("e", 8)
+king_w.goto(a, n)
 
 king_b.penup()
-king_b.goto(boardbox["e1"]["cordinates"][0], boardbox["e1"]["cordinates"][1])
+a, n = square_to_xy("e", 1)
+king_b.goto(a, n)
 
 queen_w.penup()
-queen_w.goto(boardbox["d8"]["cordinates"][0], boardbox["d8"]["cordinates"][1])
+a, n = square_to_xy("d", 8)
+queen_w.goto(a, n)
 
 queen_b.penup()
-queen_b.goto(boardbox["d1"]["cordinates"][0], boardbox["d1"]["cordinates"][1])
+a, n = square_to_xy("d", 1)
+queen_b.goto(a, n)
+
 
 t.update()
 t.done()
